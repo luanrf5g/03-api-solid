34a07d6df765f05f4b2eabce652d2d87a4f8899f
luanrf5g <of.luan.rferreira@gmail.com>
2024-07-10 06:22:43 -0300

    create first unit test with vitest

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -2,6 +2,7 @@
 import { hash } from 'bcryptjs'
 import { UsersRepositories } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterUseCasesRequest {
   name: string
@@ -9,10 +10,14 @@ interface RegisterUseCasesRequest {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(public usersRepository: UsersRepositories) {}
 
-  async execute({ name, email, password }: RegisterUseCasesRequest) {
+  async execute({ name, email, password }: RegisterUseCasesRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,10 +26,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user
+    }
   }
 }
